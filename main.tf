f340202d7315e1d368f2453f6fab60d16c683a68
runAnwesh <anweshkm1122@gmail.com>
2024-11-06 10:46:09 +0530

    CI/CD pipeline push

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   name    = "k8s-vpc"
   cidr    = "10.0.0.0/16"
-  azs     = ["us-west-2a", "us-west-2b"]
+  azs     = ["ap-south-1"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
